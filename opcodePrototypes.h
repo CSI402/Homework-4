7fe263010b787ec88a7958cd8008b105a006e67c
Jessica Kanczura <jkanczura@albany.edu>
2017-11-13 18:49:44 -0500

    Update and rename prototypes.h to opcodePrototypes.h

--- opcodePrototypes.h
+++ opcodePrototypes.h
@@ -3,5 +3,5 @@ Daniel Hug dhug@albany.edu: Monitor
 Alana Ruth Aruth@albany.edu : Recorder
 Jessica Kanczura jKanczura@albany.edu : Leader
 
-Prototypes used in functions
+Prototypes used in functions related to the opcode table
 */

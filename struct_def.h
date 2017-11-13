dbd5a930df2eb7a01915cedc646a76a799c8e75e
Jessica Kanczura <jkanczura@albany.edu>
2017-11-13 18:47:04 -0500

    Create struct_def.h

---
+++ struct_def.h
@@ -0,0 +1,8 @@
+/*
+Daniel Hug dhug@albany.edu: Monitor
+Alana Ruth Aruth@albany.edu : Recorder
+Jessica Kanczura jKanczura@albany.edu : Leader
+
+
+Struct definitions for binary search tree for machine opcode table
+*/

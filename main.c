a07c4f59199efda10f360453df9ec91cac798141
Jessica Kanczura <jkanczura@albany.edu>
2017-11-13 18:45:35 -0500

    started main

---
+++ main.c
@@ -0,0 +1,13 @@
+/*
+Daniel Hug dhug@albany.edu: Monitor
+Alana Ruth Aruth@albany.edu : Recorder
+Jessica Kanczura jKanczura@albany.edu : Leader
+
+Main method
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+  
+}

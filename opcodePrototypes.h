6bf6ec8f4734d1732ccdc7c5e7203206226b9531
Jessica Kanczura <jkanczura@albany.edu>
2017-11-13 19:34:26 -0500

    Update opcodePrototypes.h

--- opcodePrototypes.h
+++ opcodePrototypes.h
@@ -5,3 +5,9 @@ Jessica Kanczura jKanczura@albany.edu : Leader
 
 Prototypes used in functions related to the opcode table
 */
+
+//these prototypes are only to be used for opcodeFunctions.c
+void insertOpcodeNode();
+void retrieveOpcode(char *instruction[]);
+void retrieveInstructionFormat(char *instruction[]);
+void printTree();

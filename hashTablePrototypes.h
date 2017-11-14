9e5866e8c7b283bea23df76baa1ce5772dee8bd3
Jessica Kanczura <jkanczura@albany.edu>
2017-11-13 19:22:59 -0500

    added prototypes for existing functions

--- hashTablePrototypes.h
+++ hashTablePrototypes.h
@@ -6,3 +6,7 @@ Jessica Kanczura jKanczura@albany.edu : Leader
 Prototypes used in functions related to the hash table
 */
 
+//these prototypes are only to be used in hashTableFunctions.c
+void insertNode(); 
+void retrieveLC(char *symbol[]);
+void printTable();

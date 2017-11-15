a20bac232be509eb75faf1092fa5f38131a1fd58
Jessica Kanczura <jkanczura@albany.edu>
2017-11-14 22:00:49 -0500

    creating struct for BST nodes

--- opcodeStruct_def.h
+++ opcodeStruct_def.h
@@ -6,3 +6,10 @@ Jessica Kanczura jKanczura@albany.edu : Leader
 
 Struct definitions for binary search tree for machine opcode table
 */
+
+typedef struct opcodeNode{
+  char *key;
+  char *data;
+  struct opcodeNode *left;
+  struct opcodeNode *right;
+} oNode;

13e148fe67c7b4a8ab8060f18e4d1caba889dde8
Jessica Kanczura <jkanczura@albany.edu>
2017-11-17 21:14:16 -0500

    Create externs.h

---
+++ externs.h
@@ -0,0 +1,9 @@
+/*
+Daniel Hug dhug@albany.edu: Monitor
+Alana Ruth Aruth@albany.edu : Recorder
+Jessica Kanczura jKanczura@albany.edu : Leader
+
+Prototypes used in functions related to the opcode table
+*/
+
+extern oNode *oHead;
